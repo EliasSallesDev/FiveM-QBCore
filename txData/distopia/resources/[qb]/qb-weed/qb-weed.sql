0a6d1a79fd9a6aa8932d7f7257d9319630074533
Elias Sales <eliascs39comp08@gmail.com>
2026-05-06 00:59:39 -0300

    Atualizando SQL

--- txData/distopia/resources/[qb]/qb-weed/qb-weed.sql
+++ txData/distopia/resources/[qb]/qb-weed/qb-weed.sql
@@ -12,9 +12,8 @@ CREATE TABLE IF NOT EXISTS `house_plants` (
   PRIMARY KEY (`id`),
   KEY `building` (`building`),
   KEY `plantid` (`plantid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Update January 15, 2024
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Converter texto para número
 UPDATE `house_plants` SET `stage` = 1 WHERE `stage` = 'stage-a';
 UPDATE `house_plants` SET `stage` = 2 WHERE `stage` = 'stage-b';
 UPDATE `house_plants` SET `stage` = 3 WHERE `stage` = 'stage-c';
@@ -22,4 +21,6 @@ UPDATE `house_plants` SET `stage` = 4 WHERE `stage` = 'stage-d';
 UPDATE `house_plants` SET `stage` = 5 WHERE `stage` = 'stage-e';
 UPDATE `house_plants` SET `stage` = 6 WHERE `stage` = 'stage-f';
 UPDATE `house_plants` SET `stage` = 7 WHERE `stage` = 'stage-g';
+
+-- Depois converte a coluna
 ALTER TABLE `house_plants` MODIFY COLUMN `stage` int(11) DEFAULT 1;
